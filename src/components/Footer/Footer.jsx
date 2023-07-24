4f6513b101ae77f56dabe19c5d492c4078b46d14
Femi Gabriel <timfemi@gmail.com>
2023-07-24 15:02:24 +0100

    first commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -72,10 +72,10 @@ const Footer = () => {
           <Col lg="3" md="4" sm="6">
             <div className="mb-4">
               <h5 className="footer__link-title mb-4">Head Office</h5>
-              <p className="office__info">123 Zindabazar, Sylhet, Bangladesh</p>
-              <p className="office__info">Phone: +0995345875365</p>
+              <p className="office__info">123 Lekki, Lagos</p>
+              <p className="office__info">Phone: +2349061513696</p>
 
-              <p className="office__info">Email: muhib5532@gmail.com</p>
+              <p className="office__info">Email: timfemi@gmail.com</p>
 
               <p className="office__info">Office Time: 10am - 7pm</p>
             </div>
